d290a562c7631d7feafd0efc60516cc0e6e424fd
Iryna Finenko <irynafinenko@gmail.com>
2025-07-05 14:53:06 +0300

    fix all changes

---
+++ src/main/java/core/basesyntax/Ball.java
@@ -0,0 +1,15 @@
+package core.basesyntax;
+
+public class Ball {
+    final String color;
+    final int number;
+
+    public Ball(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+        @Override
+        public String toString() {
+            return "Ball{color='" + color + "', number=" + number + '}';
+        }
+    }
